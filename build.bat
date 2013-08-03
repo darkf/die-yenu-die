221e393bb0e1ff10357b63f111de8f7047349057
darkf <lw9k123@gmail.com>
2013-08-03 07:31:02 -0700

    move Enemy and related into src/enemy.ts

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-@tsc --out main.js lib\heart.d.ts src\tile.ts src\actor.ts src\main.ts
+@tsc --out main.js lib\heart.d.ts src\tile.ts src\actor.ts src\enemy.ts src\main.ts
---
+++ src/enemy.ts
@@ -0,0 +1,41 @@
+// Contains definitions and logic for enemies
+
+// Base enemy class
+
+class Enemy extends Actor {
+	constructor(x:number, level:number=1) {
+		super(x, level);
+	}
+
+	getXPDropped() {
+		return this.level * this.baseXPDrop
+	}
+
+	die() {
+		super.die();
+		player.gainXP(this.getXPDropped());
+	}
+
+	turn() {
+		super.turn();
+		if(!this.alive) return;
+
+		// see if we can attack the player
+		if(distance(this.x, player.x) <= this.spell.range) {
+				//player.attacked(this);
+				this.cast(player);
+		}
+		else {
+			// move enemies left towards player
+			if(this.x > 0 && !map.tiles[this.x-1][0].isSolid() && player.x != this.x-1) {
+				this.move(this.x-1);
+			}
+		}
+	}
+}
+
+// Enemies in the game
+
+class Zombie extends Enemy {
+	getImage() { return tile_zombie; }
+}
